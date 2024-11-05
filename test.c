68003d2f6dc9175e283a54cb98c5845d486ed7ad
katelyn <katelyn.ommi@gmail.com>
2024-11-04 19:52:31 -0500

    I like test

---
+++ test.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+    printf("Hello World.");
+    printf("Peter Drewes, my coding teacher, is a very bad bad bad bad bad bad man and he deserves an F on all his grades even tho he's done with school");
+}
